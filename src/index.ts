daf73aee2581850c4a549d3b171458b031512070
Thinh Nguyen <miyarin93@gmail.com>
2021-06-19 16:08:02 +0700

    feat(config): cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import * as dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -13,6 +14,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+/* const allowlist = ['http://localhost:4200', 'http://localhost:3000'];
+const corsOptionsDelegate = (req: Request, callback: any) => {
+  let corsOptions;
+  if (allowlist.includes(req.header('Origin') || '')) {
+    corsOptions = { origin: true }; // enable CORS
+  } else {
+    corsOptions = { origin: false }; // disable CORS
+  }
+  // const otherOptions = {
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   allowedHeaders: [
+  //     'Origin',
+  //     'X-Requested-With',
+  //     'Content-Type',
+  //     'Accept',
+  //     'X-Access-Token',
+  //   ],
+  //   credentials: true,
+  //   preflightContinue: true,
+  //   optionsSuccessStatus: 200
+  // };
+  // corsOptions = { ...corsOptions, ...otherOptions };
+  callback(null, corsOptions);
+}
+app.use(cors(corsOptionsDelegate)); */
+app.use(cors());
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   return res.status(200).json({
